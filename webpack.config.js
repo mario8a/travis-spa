8a701b87ae039463d379c173d897301c127f58db
mario8a <mario8ato@hotmail.com>
2021-07-17 16:47:40 -0500

    fix versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
-      }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module:{
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
+            }
+        ])
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin([
-      {
-        inject: true,
-        template: './public/index.html',
-        filename: './index.html',
-      }
-    ]),
-  ]
+
 }
